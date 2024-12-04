506937e44ada788340695f3928d1449c3902a09e
vasek1 <cistecky121@trebesin.cz>
2024-12-04 09:33:04 +0100

    maturita

--- README.md
+++
@@ -1,6 +0,0 @@
-# Úkol flask
-
-- Popište kód
-- Popište strukturu (static, template)
-- Kam patří form.html?
-- Vytvořte úvodní stránku s nadpisem a zajistěte, ať se zobrazí při zapnutí webové aplikace
--- app.py
+++ app.py
@@ -1,30 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# do template se dává všechno html a do static zbytek obrázky a css 
-#form patří do template
+
 @app.route("/")
-def vitej():
+def home():
     return render_template("start.html")        
 
-
- #name = request.args.get("name") 
- #input_class = request.args.get("class")
- #message = request.args.get("message")
- #return redirect(url_for("result", name=name, form_class=input_class, message=message))
-    
 @app.route("/form")
 def form():
- name = request.form.get("name") #z formuláře vezme jméno
- input_class = request.form.get("class")# z formuláře vezme clasu
- message = request.form.get("message") #z formuláře vezme message
- return redirect(url_for("result", name=name, form_class=input_class, message=message))
-            #a vypíše je v html result
-
-
-
-
+ py_var = request.args.get("html_name")
+ return render_template("form.html", jinja_var = py_var)
 
 if __name__ == "__main__":
-    app.run(debug=True) #spouštění flaskové aplikace
+    app.run(debug=True) 
